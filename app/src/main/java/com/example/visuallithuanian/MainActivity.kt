5514be22351aaf252a7ed13b811d8a5542829f3f
klaus19 <tejaskhartude@gmail.com>
2022-09-27 19:49:00 +0530

    updated

--- app/src/main/java/com/example/visuallithuanian/MainActivity.kt
+++ app/src/main/java/com/example/visuallithuanian/MainActivity.kt
@@ -3,22 +3,26 @@ package com.example.visuallithuanian
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    val activityScope = CoroutineScope(Dispatchers.Main)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        CoroutineScope(Dispatchers.Main).launch {
+        activityScope.launch {
 
             delay(1000)
             val intent = Intent(this@MainActivity,FirstScreen::class.java)
             startActivity(intent)
             finish()
         }
+    }
+
+    override fun onPause() {
+        activityScope.cancel()
+        super.onPause()
     }
 }
