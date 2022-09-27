dc6ec0933db82de3e8df75e6055203c3f6966266
klaus19 <tejaskhartude@gmail.com>
2022-09-27 16:40:15 +0530

    Added Splashscreen to the App using Kotlin Coroutines

---
+++ app/src/main/java/com/example/visuallithuanian/FirstScreen.kt
@@ -0,0 +1,13 @@
+package com.example.visuallithuanian
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FirstScreen:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+}
--- app/src/main/java/com/example/visuallithuanian/MainActivity.kt
+++ app/src/main/java/com/example/visuallithuanian/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.visuallithuanian
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CoroutineScope(Dispatchers.Main).launch {
+
+            delay(1000)
+            val intent = Intent(this@MainActivity,FirstScreen::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
     }
 }
