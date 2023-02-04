7183314d661776c2e67fb37233baa40e9e3bea56
klaus19 <tejaskhartude@gmail.com>
2023-02-04 16:53:51 +0530

    updated navigation graph

--- app/src/main/java/com/example/visuallithuanian/PractiseFragment.kt
+++ app/src/main/java/com/example/visuallithuanian/PractiseFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [LocationFragment.newInstance] factory method to
+ * Use the [PractiseFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class LocationFragment : Fragment() {
+class PractiseFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class LocationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false)
+        return inflater.inflate(R.layout.fragment_practise, container, false)
     }
 
     companion object {
@@ -49,7 +49,7 @@ class LocationFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            LocationFragment().apply {
+            PractiseFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
