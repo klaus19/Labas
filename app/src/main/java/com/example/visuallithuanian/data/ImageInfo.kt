5789a237ac8368fa848eb0f43f2107c38bb6d8f3
klaus19 <tejaskhartude@gmail.com>
2023-02-06 22:58:57 +0530

    made chnages in cardview by adding voice icons and power icon

--- app/src/main/java/com/example/visuallithuanian/data/ImageInfo.kt
+++ app/src/main/java/com/example/visuallithuanian/data/ImageInfo.kt
@@ -1,4 +1,4 @@
 package com.example.visuallithuanian.data
 
-data class ImageInfo(val imageId:Int,val name1:String,val name2:String)
+data class ImageInfo(val imageId:Int, val name1:String, val name2:String, var list:List<Int>,val voice:Int)
 
