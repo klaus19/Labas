2709f435b200ce5b8319c5ec6aeacec6ebadfb5c
klaus19 <tejaskhartude@gmail.com>
2023-04-05 09:53:16 +0530

    completed the birds section

--- app/src/main/java/com/example/visuallithuanian/BirdsFragment.kt
+++ app/src/main/java/com/example/visuallithuanian/BirdsFragment.kt
@@ -12,9 +12,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.visuallithuanian.adapter.ImageAdapter
 import com.example.visuallithuanian.data.ImageInfo
+import com.example.visuallithuanian.databinding.FragmentBirdsBinding
 import com.example.visuallithuanian.ui.activities.FirstScreen
 import com.example.visuallithuanian.viewModel.BottomNavigationViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -24,17 +26,19 @@ class BirdsFragment : Fragment() {
     lateinit var viewModel: BottomNavigationViewModel
     lateinit var bottomNavigationView: BottomNavigationView
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        // Inflate the layout for this fragment
-        val view =  inflater.inflate(R.layout.fragment_directions, container, false)
+
+        val view =  inflater.inflate(R.layout.fragment_birds, container, false)
 
         bottomNavigationView = (activity as? FirstScreen)?.findViewById(R.id.bottomNavigationView)!!
         viewModel = ViewModelProvider(requireActivity()).get(BottomNavigationViewModel::class.java)
         val recyclerView = view?.findViewById<RecyclerView>(R.id.recyclerViewAnimals)
+        val fb1 = view?.findViewById<FloatingActionButton>(R.id.floatingActionButton)
 
         // Added a functionality where the bottomnavigation view will get invisible while scrolling and
         // appear after scrolling is stopped
@@ -43,8 +47,8 @@ class BirdsFragment : Fragment() {
 
         recyclerView?.layoutManager = LinearLayoutManager(context)
 
-        val back_icon = view.findViewById<ImageView>(R.id.back_icon)
-        back_icon.setOnClickListener {
+        val back_icon = view?.findViewById<ImageView>(R.id.back_icon)
+        back_icon?.setOnClickListener {
             findNavController().navigate(R.id.action_birdsFragment_to_userFragment)
         }
 
@@ -53,11 +57,39 @@ class BirdsFragment : Fragment() {
         // settingup ImageAdapter
         val adapter = ImageAdapter(birdList)
         recyclerView?.adapter = adapter
+
+        fb1?.setOnClickListener {
+            findNavController().navigate(R.id.action_birdsFragment_to_flashCards)
+        }
+
         return view
     }
 
     private fun generateBirdsList(): List<ImageInfo> {
-        TODO("Not yet implemented")
+        return listOf(
+            ImageInfo(R.drawable.whitestork,"White Stork","Baltasis gandrovas",listOf(R.drawable.purp), R.drawable.mic,R.raw.potato,
+                "",""),
+            ImageInfo(R.drawable.crane,"Common Crane","Paprastasis gegužė",listOf(R.drawable.purp), R.drawable.mic,R.raw.potato,
+                "",""),
+            ImageInfo(R.drawable.blackgrouse,"Black Grouse","Tetervinas",listOf(R.drawable.purp), R.drawable.mic,R.raw.potato,
+                "",""),
+            ImageInfo(R.drawable.greentit,"Great Tit","Didysis Meškažirnis",listOf(R.drawable.purp), R.drawable.mic,R.raw.potato,
+                "",""),
+            ImageInfo(R.drawable.chaff,"Common Chaffinch","Paprastasis šernas",listOf(R.drawable.purp), R.drawable.mic,R.raw.potato,
+                "",""),
+            ImageInfo(R.drawable.jay,"Eurasian Jay","Žvirblys",listOf(R.drawable.purp), R.drawable.mic,R.raw.potato,
+                "",""),
+            ImageInfo(R.drawable.robin,"European Robin","Raudonkepuraitė",listOf(R.drawable.purp), R.drawable.mic,R.raw.potato,
+                "",""),
+            ImageInfo(R.drawable.blackbird,"Common Blackbird","Juodvarnis",listOf(R.drawable.purp), R.drawable.mic,R.raw.potato,
+                "",""),
+            ImageInfo(R.drawable.buzzard,"Common Buzzard"," Paprastasis suopis",listOf(R.drawable.purp), R.drawable.mic,R.raw.potato,
+                "",""),
+
+        )
+
+
+
     }
 
 }
