f75459c85286f92303c7e8b9c7fe64519b4f33fb
klaus19 <tejaskhartude@gmail.com>
2023-07-11 17:50:48 +0530

    got back the bug

--- app/src/main/java/com/example/visuallithuanian/QuestionsFragment.kt
+++ app/src/main/java/com/example/visuallithuanian/QuestionsFragment.kt
@@ -2,7 +2,6 @@ package com.example.visuallithuanian
 
 import android.animation.*
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.Log
@@ -10,12 +9,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.example.visuallithuanian.database.FlashcardPair
 import com.example.visuallithuanian.databinding.FragmentQuestionsBinding
@@ -30,19 +26,19 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class QuestionsFragment : Fragment() {
-    lateinit var binding: FragmentQuestionsBinding
+    lateinit var binding:FragmentQuestionsBinding
     lateinit var viewModel: BottomNavigationViewModel
 
     lateinit var bottomNavigationView: BottomNavigationView
     private val counterViewModel: ToLearnViewModel by viewModels()
 
-    private val hashMap = HashMap<String,String>()
+    private val hashMap = HashMap<String,Pair<String,Int>>()
 
     private var currentPairIndex =0
-    private lateinit var currentPair:Map.Entry<String,String>
+    private lateinit var currentPair:Map.Entry<String,Pair<String,Int>>
 
     var isFront=true
-    private val totalPairs = 17 // change the value to the actual number of entries in your hashMap
+    private val totalPairs = 16 // change the value to the actual number of entries in your hashMap
 
     // declaring viewmodel
     private val cardViewModel: FlashCardViewmodel by viewModels {
@@ -78,26 +74,27 @@ class QuestionsFragment : Fragment() {
 
         //changing color of background color of progress bar
         binding.progressHorizontal.progressBackgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(requireContext(),
-        R.color.silver))
+            R.color.silver))
 
         // Hashmap of strings that will shown on cardview front and back side
-        hashMap["What"] = "Kas"
-        hashMap["When"] = "Kai"
-        hashMap["Where"] = "Kur"
-        hashMap["Who"] = "Kas"
-        hashMap["Whom"] = "Kam"
-        hashMap["Why"] = "Kodėl"
-        hashMap["How"] = "Kaip"
-        hashMap["Which"] = "Kuris/kuri"
-        hashMap["Whose"] = "Kieno"
-        hashMap["I"] = "aš"
-        hashMap["you (singular)"] = "tu/jūs (informal/formal)"
-        hashMap["he"] = "jis"
-        hashMap["she"] = "ji"
-        hashMap["it "] = "tai"
-        hashMap["we"] = "mes"
-        hashMap["you (plural)"] = "jūs"
-        hashMap["they"] = "jie"
+        hashMap["What"] = Pair("Kas", R.drawable.what)
+        hashMap["When"] = Pair("Kai",R.drawable.whennn)
+        hashMap["Where"] = Pair("Kur",R.drawable.where)
+        hashMap["Who"] = Pair("Kas",R.drawable.who)
+        hashMap["Whom"] = Pair("Kam",R.drawable.whom)
+        hashMap["Why"] = Pair("Kodėl",R.drawable.why)
+        hashMap["How"] = Pair("Kaip",R.drawable.how)
+        hashMap["Which"] = Pair("Kuris/kuri",R.drawable.which)
+        hashMap["Whose"] = Pair("Kieno",R.drawable.whose)
+        hashMap["I"] = Pair("aš",R.drawable.ii)
+        hashMap["you (singular)"] = Pair("tu/jūs (informal/formal)",R.drawable.you)
+        hashMap["he"] = Pair("jis",R.drawable.he)
+        hashMap["she"] = Pair("ji",R.drawable.she)
+        hashMap["we"] = Pair("mes",R.drawable.we)
+        hashMap["you (plural)"] = Pair("jūs",R.drawable.you)
+        hashMap["they"] = Pair("jie",R.drawable.they)
+
+
 
         counterViewModel.counter.observe(requireActivity()){count->
             binding.textCounter.text = count.toString()
@@ -107,10 +104,13 @@ class QuestionsFragment : Fragment() {
 
         currentPair = hashMap.entries.elementAt(currentPairIndex)
         binding.textCardFront.text = currentPair.key
-        binding.textCardBack.text = hashMap[currentPair.key]
+        binding.textCardBack.text = currentPair.value.first
+        binding.imagecardsHelper.setImageResource(currentPair.value.second!!)
 
         // onclick listener on the image
         binding.imageFlashCard.setOnClickListener {
+            val clickedColor = ContextCompat.getColor(requireContext(), R.color.white)
+            binding.imageFlashCard.setBackgroundColor(clickedColor)
 
             counterViewModel.incrementCounter()
             // increment currentPairIndex and get the next pair
@@ -123,15 +123,19 @@ class QuestionsFragment : Fragment() {
 
             val front = binding.textCardFront.text.toString()
             val back = binding.textCardBack.text.toString()
+            val imageHelper = currentPair.value.second
 
-            val pair = FlashcardPair(front, back)
+            val pair = FlashcardPair(front, back,imageHelper!!)
             cardViewModel.insertCards(pair)
-            Toast.makeText(requireContext(),"saved data", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(requireContext(),"saved data", Toast.LENGTH_SHORT).show()
             Log.d("Main","$pair")
 
             // update the UI with the new pair
             binding.textCardFront.text = currentPair.key
-            binding.textCardBack.text = hashMap[currentPair.key]
+            binding.textCardBack.text =currentPair.value.first
+            binding.imagecardsHelper.setImageResource(currentPair.value.second!!)
+
+
 
         }
 
@@ -147,6 +151,9 @@ class QuestionsFragment : Fragment() {
                 val progress = ((currentPairIndex + 1) * 100) / totalPairs
                 binding.progressHorizontal.progress = progress
 
+                val originalColor = ContextCompat.getColor(requireContext(), R.color.pink)
+                binding.imageFlashCard.setBackgroundColor(originalColor)
+
                 // initialize currentPairIndex to 0 if it hasn't been initialized yet
                 if (currentPairIndex < 0) {
                     currentPairIndex = 0
@@ -160,14 +167,14 @@ class QuestionsFragment : Fragment() {
                     textCardBack.visibility = View.VISIBLE
                     textCardFront.visibility = View.GONE
                     imageFlashCard.visibility = View.GONE
-                    cardViewQuestions.setCardBackgroundColor(ContextCompat.getColor(requireContext(), R.color.pink))
+                    cardViewQuestions.setCardBackgroundColor(ContextCompat.getColor(requireContext(), R.color.card_purple))
 
                 } else {
                     currentPairIndex = (currentPairIndex + 1) % hashMap.size
                     textCardFront.visibility = View.VISIBLE
                     textCardBack.visibility = View.GONE
                     imageFlashCard.visibility = View.VISIBLE
-                    cardViewQuestions.setCardBackgroundColor(ContextCompat.getColor(requireContext(), R.color.blue_card))
+                    cardViewQuestions.setCardBackgroundColor(ContextCompat.getColor(requireContext(), R.color.pink))
                     front_animation.setTarget(textCardBack)
                     back_animation.setTarget(textCardFront)
                     back_animation.start()
@@ -184,7 +191,8 @@ class QuestionsFragment : Fragment() {
                 // retrieve the current pair from the hashMap
                 currentPair = hashMap.entries.elementAt(currentPairIndex)
                 binding.textCardFront.text = currentPair.key
-                binding.textCardBack.text = hashMap[currentPair.key]
+                binding.textCardBack.text = currentPair.value.first
+                binding.imagecardsHelper.setImageResource(currentPair.value.second!!)
             }
         }
 
