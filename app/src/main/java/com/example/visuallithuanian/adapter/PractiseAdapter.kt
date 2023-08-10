195f85ff6240eb8f28ba8f9c7c6baaa9f52c83fe
klaus19 <tejaskhartude@gmail.com>
2023-08-10 18:41:39 +0530

    SOLVED A BUG ABOUT CORRECT ELEMENTS NOT GETTING CORRECT COLOUR AS BEACKGROUND

--- app/src/main/java/com/example/visuallithuanian/adapter/PractiseAdapter.kt
+++ app/src/main/java/com/example/visuallithuanian/adapter/PractiseAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.visuallithuanian.adapter
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,33 +11,79 @@ import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.visuallithuanian.R
+import com.example.visuallithuanian.constants.ImageStore
 
-class PractiseAdapter(private val imageNamesMap: HashMap<Int, String>) :
-    RecyclerView.Adapter<PractiseAdapter.PractiseViewHolder>() {
 
-    private val imagesList: MutableList<Int> = imageNamesMap.keys.toMutableList()
+class PractiseAdapter(
+    private val imageResources: List<Int>,
+    private val imageNames: List<String>
+) : RecyclerView.Adapter<PractiseAdapter.PractiseViewHolder>() {
+
+    private var selectedImageResource = -1
+    private var selectedImageName = ""
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PractiseViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_practise_cards, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_practise_cards, parent, false)
         return PractiseViewHolder(view)
     }
 
-    override fun getItemCount(): Int = imagesList.size
+    override fun getItemCount(): Int = imageResources.size
 
-    override fun onBindViewHolder(holder: PractiseViewHolder, position: Int) {
-        val imageResource = imagesList[position]
-        val imageName = imageNamesMap[imageResource]
+    override fun onBindViewHolder(holder: PractiseViewHolder, @SuppressLint("RecyclerView") position: Int) {
+
+        val imageResource = imageResources[position]
+        val imageName = imageNames[position]
 
         holder.imageViewPractise.setImageResource(imageResource)
         holder.textViewPractise.text = imageName
 
+        // Set the background color of the image card based on selection
+        holder.cardImagePractise.setCardBackgroundColor(
+            if (selectedImageResource == imageResources[position]) Color.GREEN else Color.WHITE
+        )
+
+
         holder.cardImagePractise.setOnClickListener {
-            onCardClick(it, position)
+            // Set the selected image position and reset the selected name position
+            selectedImageResource = imageResources[position]
+            selectedImageName = ""
+            notifyDataSetChanged()
         }
 
         holder.cardTextPractise.setOnClickListener {
-            onCardClick(it, position)
+            if (selectedImageResource == -1) {
+                Toast.makeText(
+                    it.context,
+                    "Please select an image card first.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                selectedImageName = imageNames[position]
+            }
+            notifyDataSetChanged()
         }
+        val nameColor = if (imageNames[position] == selectedImageName) {
+            if (ImageStore.imagesNamesMap[selectedImageResource] == selectedImageName) {
+                Toast.makeText(
+                    holder.itemView.context,
+                    "Correct name selected!",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Color.GREEN
+            } else {
+                Toast.makeText(
+                    holder.itemView.context,
+                    "Wrong name selected!",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Color.RED
+            }
+        } else {
+            Color.WHITE
+        }
+        holder.textViewPractise.setBackgroundColor(nameColor)
     }
 
     class PractiseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -43,19 +91,5 @@ class PractiseAdapter(private val imageNamesMap: HashMap<Int, String>) :
         val cardTextPractise: CardView = itemView.findViewById(R.id.cardTextPractise)
         val imageViewPractise: ImageView = itemView.findViewById(R.id.imageViewPractise)
         val textViewPractise: TextView = itemView.findViewById(R.id.textViewPractise)
-    }
-
-    private fun onCardClick(view: View, position: Int) {
-        // Check the clicked view's ID to identify which cardView was clicked
-        when (view.id) {
-            R.id.cardImagePractise -> {
-                // Handle image card click
-                Toast.makeText(view.context, "Image clicked at position $position", Toast.LENGTH_SHORT).show()
-            }
-            R.id.cardTextPractise -> {
-                // Handle text card click
-                Toast.makeText(view.context, "Text clicked at position $position", Toast.LENGTH_SHORT).show()
-            }
-        }
     }
 }
